35c51342576d5b64ed14739768ea77cf0b4cc91d
James Dabbs <jamesdabbs@gmail.com>
2016-06-01 09:43:09 -0400

    Use sinatra/base

--- app.rb
+++ app.rb
@@ -1,11 +1,18 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'pry'
 
-get "/hello" do
-  "hello world"
+class App < Sinatra::Base
+  # 5.times do
+  #   puts "hello"
+  # end
+  get "/hello" do
+    "hello world"
+  end
+
+  get "/hello/:name" do
+    name = params[:name]
+    "hello, #{name}"
+  end
 end
 
-get "/hello/:name" do
-  name = params[:name]
-  "hello, #{name}"
-end
+App.run!
